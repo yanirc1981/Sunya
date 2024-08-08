ea926145020719c9d09ea563d297168a04c74de3
yanirc1981 <cerzurc@hotmail.com>
2024-08-08 15:55:56 -0300

    se elimina carpeta.vit que se repite

--- Sunya_Fe/.vite/deps/react-dom.js
+++
@@ -1,7 +0,0 @@
-import {
-  require_react_dom
-} from "./chunk-L2PNZYJC.js";
-import "./chunk-C4GATGNJ.js";
-import "./chunk-DC5AMYBS.js";
-export default require_react_dom();
-//# sourceMappingURL=react-dom.js.map
