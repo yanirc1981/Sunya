bbca57296f36c7014b294db90da5964ef464ce91
yanirc1981 <cerzurc@hotmail.com>
2024-08-11 19:25:59 -0300

    Cards

--- Sunya_Fe/src/Components/Cards/Cards.jsx
+++ Sunya_Fe/src/Components/Cards/Cards.jsx
@@ -1,76 +1,78 @@
 import { useState } from 'react';
-import Carousel from 'react-bootstrap/Carousel';
+import { Link } from 'react-router-dom';
 import ImageComponent from '../ImageComponent/ImageComponent';
 import ImageComponentA from '../ImageComponent/ImageComponentA';
 import ImageComponentB from '../ImageComponent/ImageComponentB';
 import ImageComponentC from '../ImageComponent/ImageComponentC';
-import './cards.css';
-import { Link } from 'react-router-dom';
 
 function Cards() {
   const [index, setIndex] = useState(0);
 
-  const handleSelect = (selectedIndex) => {
-    setIndex(selectedIndex);
+  const images = [
+    {
+      component: <ImageComponent text="First slide" />,
+      title: "LÁCTEOS",
+      subtitle: "Yogurt, Kumis, Avena",
+    },
+    {
+      component: <ImageComponent text="Third slide" />,
+      title: "QUESOS",
+      subtitle: "Frescos y Maduros",
+    },
+    {
+      component: <ImageComponent text="Fourth slide" />,
+      title: "POSTRES",
+      subtitle: "Frescos y Deliciosos",
+    },
+  ];
+
+  const handleNext = () => {
+    setIndex((prevIndex) => (prevIndex + 1) % images.length);
+  };
+
+  const handlePrev = () => {
+    setIndex((prevIndex) => (prevIndex - 1 + images.length) % images.length);
   };
 
   return (
-    <div className="cards-container">
-      <div className="carousel-with-title">
-        <h2 className="carousel-title">Productos</h2>
-        <Carousel
-          activeIndex={index}
-          onSelect={handleSelect}
-          className="carousel_container"
-          prevIcon={
-            <span className="carousel-control-prev-icon" aria-hidden="true" />
-          }
-          nextIcon={
-            <span className="carousel-control-next-icon" aria-hidden="true" />
-          }
+    <div className="relative w-full h-screen overflow-hidden">
+      <div className="absolute inset-0 flex justify-between items-center px-4 z-10">
+        <button
+          onClick={handlePrev}
+          className="bg-white bg-opacity-50 hover:bg-opacity-75 text-black font-bold py-2 px-4 rounded-full focus:outline-none"
         >
-          <Carousel.Item className="div_image">
-            <ImageComponent text="First slide" />
-            <Carousel.Caption>
-            <Link to="/products">
-              <div className="item-title">LÁCTEOS
-              <div className="subtitles">
-                  <span>Yogurt, Kumis, Avena</span>
-                </div>
-              </div>
-              </Link>
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item className="div_image">
-            <ImageComponent text="Third slide" />
-            <Carousel.Caption>
-            <Link to="/products">
-              <div className="item-title">
-                QUESOS
-                <div className="subtitles">
-                  <span>Frescos y Maduros</span>
-                </div>
-              </div>
-              </Link>
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item className="div_image">
-            <ImageComponent text="Fourth slide" />
-            <Carousel.Caption>
-            <Link to="/products">
-              <div className="item-title">
-                POSTRES
-                <div className="subtitles">
-                  <span>Frescos y Deliciosos</span>
-                </div>
-              </div>
-              </Link>
-            </Carousel.Caption>
-          </Carousel.Item>
-        </Carousel>
+          &#8249;
+        </button>
+        <button
+          onClick={handleNext}
+          className="bg-white bg-opacity-50 hover:bg-opacity-75 text-black font-bold py-2 px-4 rounded-full focus:outline-none"
+        >
+          &#8250;
+        </button>
       </div>
+      {images.map((image, i) => (
+        <div
+          key={i}
+          className={`absolute inset-0 transition-transform duration-500 ease-in-out ${
+            i === index ? 'translate-x-0' : 'translate-x-full'
+          }`}
+          style={{ display: i === index ? 'block' : 'none' }}
+        >
+          <div className="w-full h-full relative">
+            {image.component}
+            <div className="absolute inset-0 flex flex-col justify-center items-center bg-black bg-opacity-50 text-white">
+              <Link to="/products" className="text-center">
+                <div className="text-4xl font-bold">{image.title}</div>
+                <div className="text-lg mt-2">{image.subtitle}</div>
+              </Link>
+            </div>
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
 
 export default Cards;
+
+
